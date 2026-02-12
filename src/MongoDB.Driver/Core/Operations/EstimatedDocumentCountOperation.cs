1545014ef2f392ee7ec7d764516bb537206d28d2
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2026-02-12 11:28:33 +0100

    Fixes for tests

--- src/MongoDB.Driver/Core/Operations/EstimatedDocumentCountOperation.cs
+++ src/MongoDB.Driver/Core/Operations/EstimatedDocumentCountOperation.cs
@@ -92,7 +92,7 @@ namespace MongoDB.Driver.Core.Operations
             }
         }
 
-        private IDisposable BeginOperation() => EventContext.BeginOperation("count");
+        private IDisposable BeginOperation() => EventContext.BeginOperation(null, "count");   //TODO Need to investigate better this
 
         private IExecutableInRetryableReadContext<long> CreateCountOperation()
         {
